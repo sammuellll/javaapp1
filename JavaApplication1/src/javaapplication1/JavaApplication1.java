4748b0466e0ba18cd6d1136997b046b62c6323c8
sammuellll <ibrahim.sayhan.86@gmail.com>
2020-03-30 13:56:47 +0200

    javaapp1 update 0.00001
    javaapp1 update 0.00001

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,36 @@
+
+package javaapplication1;
+
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+    int a = 4;
+        int b= 5;
+        int c=6;
+        int toplam ;
+        toplam = a+2*b+3*c;
+        
+        System.out.println("Toplam = " + toplam);
+        System.out.println(3+4.2);
+        System.out.println(3-5f);
+        System.out.println(10/4);
+        System.out.println(10/4.0);
+        System.out.println(3*4.5);
+        System.out.println(10%3);
+        System.out.println(11%4);
+        int i =1 ;
+        i += 2 ; //i=i+2
+        i *=3 ;  //i=i*3
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        i--;
+        System.out.println(i);
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        System.out.println("5f/2+3*7 = " + (5f/2+3*7));
+    }
+    
+}
